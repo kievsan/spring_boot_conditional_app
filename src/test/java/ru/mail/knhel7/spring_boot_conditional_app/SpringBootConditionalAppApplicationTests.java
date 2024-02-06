4a4f238e98162bfe8c61507f513f02853e662b7a
kievskiy <knhel7@mail.ru>
2024-02-06 16:06:00 +0300

    The tests are implemented.

--- src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
+++ src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
@@ -9,11 +9,20 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootConditionalAppApplicationTests {
+
+	private final static Map<String, Integer> containers = new HashMap<>();
+	static {
+		containers.put("dev_app", 8080);
+		containers.put("prod_app", 8081);
+	}
 
 	String result(Integer port) {
 		String targetURL = "http://localhost:" + port + "/profile";
@@ -31,21 +40,21 @@ class SpringBootConditionalAppApplicationTests {
 
 	@Container
 	public final static GenericContainer<?> devApp = new GenericContainer<>("dev_app")
-			.withExposedPorts(8080);
+			.withExposedPorts(containers.get("dev_app"));
 
 	@Container
 	public final static GenericContainer<?> prodApp = new GenericContainer<>("prod_app")
-			.withExposedPorts(8081);
+			.withExposedPorts(containers.get("prod_app"));
 
 	@Test
 	void contextLoadsDevApp() {
-		final Integer port = devApp.getMappedPort(8080);
+		final Integer port = devApp.getMappedPort(containers.get("dev_app"));
 		assertEquals("Current profile is dev", result(port));
 	}
 
 	@Test
 	void contextLoadsProdApp() {
-		final Integer port = prodApp.getMappedPort(8081);
+		final Integer port = prodApp.getMappedPort(containers.get("prod_app"));
 		assertEquals("Current profile is production", result(port));
 	}
 
