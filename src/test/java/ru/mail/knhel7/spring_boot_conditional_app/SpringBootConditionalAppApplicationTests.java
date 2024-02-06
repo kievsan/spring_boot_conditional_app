f769bd4d028f00939d11125b5e2e7e9b95b64e7d
kievskiy <knhel7@mail.ru>
2024-02-06 13:28:43 +0300

    tests added

--- src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
+++ src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
@@ -1,39 +1,45 @@
 package ru.mail.knhel7.spring_boot_conditional_app;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootConditionalAppApplicationTests {
 
 	@Autowired
-	TestRestTemplate restTemplate;
-	public static GenericContainer<?> devapp = new GenericContainer<>("devapp")
+	TestRestTemplate testTemplate;
+
+	@Container
+	public final static GenericContainer<?> devApp = new GenericContainer<>("devapp")
 			.withExposedPorts(8080);
-	public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp")
+
+	@Container
+	public final static GenericContainer<?> prodApp = new GenericContainer<>("prodapp")
 			.withExposedPorts(8081);
-
-
-	@BeforeAll
-	public static void setUp() {
-		devapp.start();
-		prodapp.start();
-	}
 
 	@Test
 	void contextLoadsDevApp() {
-		ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
-		System.out.println(forEntity.getBody());
+		ResponseEntity<String> respEntity = testTemplate.getForEntity(
+				"http://localhost:" + devApp.getMappedPort(8080),
+				String.class);
+		String answer = respEntity.getBody();
+		System.out.println(answer);
 	}
+
 	@Test
 	void contextLoadsProdApp() {
-		ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + prodapp.getMappedPort(8080), String.class);
-		System.out.println(forEntity.getBody());
+		ResponseEntity<String> respEntity = testTemplate.getForEntity(
+				"http://localhost:" + prodApp.getMappedPort(8080),
+				String.class);
+		String answer = respEntity.getBody();
+		System.out.println(answer);
 	}
 
 }
