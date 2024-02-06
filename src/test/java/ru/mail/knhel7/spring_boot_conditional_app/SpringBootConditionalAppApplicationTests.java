2838e4062acbddfed449ae052dc3d605930c13b6
kievskiy <knhel7@mail.ru>
2024-02-06 15:21:47 +0300

    The tests are implemented.

--- src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
+++ src/test/java/ru/mail/knhel7/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
@@ -9,9 +9,19 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootConditionalAppApplicationTests {
+
+	String result(Integer port) {
+		String targetURL = "http://localhost:" + port + "/profile";
+		ResponseEntity<String> respEntity = testTemplate.getForEntity(targetURL, String.class);
+		final String result = respEntity.getBody();
+		System.out.println(targetURL + ": " + result);
+		return result;
+	}
 
 	@Autowired
 	TestRestTemplate testTemplate;
@@ -30,19 +40,15 @@ class SpringBootConditionalAppApplicationTests {
 	@Test
 	void contextLoadsDevApp() {
 		Integer port = devApp.getMappedPort(8080);
-		String devURL = "http://localhost:" + port + "/profile";
-		ResponseEntity<String> respEntity = testTemplate.getForEntity(devURL, String.class);
-		String answer = respEntity.getBody();
-		System.out.println(devURL + ": " + answer);
+		final String expected = "Current profile is dev";
+		assertEquals(expected, result(port));
 	}
 
 	@Test
 	void contextLoadsProdApp() {
 		Integer port = prodApp.getMappedPort(8081);
-		String devURL = "http://localhost:" + port + "/profile";
-		ResponseEntity<String> respEntity = testTemplate.getForEntity(devURL, String.class);
-		String answer = respEntity.getBody();
-		System.out.println(devURL + ": " + answer);
+		final String expected = "Current profile is production";
+		assertEquals(expected, result(port));
 	}
 
 }
